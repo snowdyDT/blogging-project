0a94d215e8cf0c0fe9c2cf9e90d9372b02ea9e3d
Sabyr <shatekov.sabyr@gmail.com>
2023-12-03 15:58:23 +0600

    added specifically manager PublishedManager

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,12 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (super().get_queryset()
+                .filter(status=Post.Status.PUBLISHED))
+
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -21,6 +27,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+
+    objects = models.Manager()  # default manager
+    published = PublishedManager()  # specifically manager
 
     class Meta:
         ordering = ['-publish']
